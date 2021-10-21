afb8a8e0d3a94af65b4258f305bc737b2459b1ea
Ales Ragnel <alesrangel15@gmail.com>
2021-10-21 20:47:25 -0300

    backend

---
+++ backend/src/main/java/br/com/aterrissar/aterrissar/AterrissarApplication.java
@@ -0,0 +1,13 @@
+package br.com.aterrissar.aterrissar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AterrissarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AterrissarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/aterrissar/aterrissar/AterrissarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aterrissar.aterrissar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AterrissarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
